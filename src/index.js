aadf76e11dfdc2acdeffbda6173903fb822dd767
Acool4ik <byshkov.oleg@yandex.ru>
2021-02-22 19:13:32 +0300

    added post css and css modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './index.css'
 import json from './assets/json/data.json'
 import swg from '@assets/images/extra-light.svg'
 import jpeg from '@assets/images/1x/1.jpg'
+import styles from './component.module.css'
+
+
+console.log(styles);
 
 
 console.log(swg);
@@ -9,3 +13,10 @@ console.log(jpeg);
 
 console.log('Hello world 3');
 console.log(json);
+
+
+const p = document.createElement('p')
+p.textContent = 'edhviuewjvwev'
+p.classList.add(styles.logo)
+
+document.body.insertAdjacentElement('afterbegin', p)
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,38 @@
 const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+const HTMLWebpackPlugin              = require('html-webpack-plugin')
+const {CleanWebpackPlugin}           = require('clean-webpack-plugin')
+const CopyWebpackPlugin              = require('copy-webpack-plugin')
+const MiniCssExtractPlugin           = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin            = require('terser-webpack-plugin')
+const autoprefixer                   = require('autoprefixer')
+const postCssModulesValues           = require('postcss-modules-values')
+
 
 const options_HTMLWebpackPlugin = {template: './index.html'}
-const options_CopyWebpackPlugin = {
-    patterns: [
-        {
-            from: path.resolve(__dirname, 'src/favicon.ico'),
-            to: path.resolve(__dirname, 'build')
-        }
-    ]
+const options_CopyWebpackPlugin = {patterns: [{
+    from: path.resolve(__dirname, 'src/favicon.ico'),
+    to: path.resolve(__dirname, 'build')
+}]}
+const options_MiniCssExtractPlugin = {filename: 'stories.css'}
+const options_postCssLoader =  {plugins: ["postcss-preset-env", autoprefixer(), postCssModulesValues]}
+
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const optimization = () => {
+    if(!isDevelopment) return {minimize: true, minimizer: [
+        new OptimizeCssAssetsWebpackPlugin(),
+        new TerserWebpackPlugin()
+    ]}
+    // {splitChunks: {chunks: 'all'}} // dont needed
+    return {}
 }
 
+
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
 
     context: path.resolve(__dirname, 'src'),
 
@@ -25,14 +43,15 @@ module.exports = {
 
     output: 
     {
-        filename: '[name].bundle.js',
+        filename: 'stories.js',
         path: path.resolve(__dirname, 'build')
     },
 
     resolve: 
     {
         extensions: ['.js', '.json', '.png', '.jpg', '.svg', '.ttf'],
-        alias: {
+        alias: 
+        {
             '@stateManager': path.resolve(__dirname, 'src/stateManager'),
             '@routeManager': path.resolve(__dirname, 'src/routeManager'),
             '@assets': path.resolve(__dirname, 'src/assets'),
@@ -40,25 +59,21 @@ module.exports = {
         }
     },
 
-    optimization: 
-    {
-        splitChunks: 
-        {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
 
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
         compress: true,
-        port: 8080
+        port: 8080,
+        hot: isDevelopment
     },
 
     plugins: 
     [
         new HTMLWebpackPlugin(options_HTMLWebpackPlugin),
         new CleanWebpackPlugin(),
-        new CopyWebpackPlugin(options_CopyWebpackPlugin)
+        new CopyWebpackPlugin(options_CopyWebpackPlugin),
+        new MiniCssExtractPlugin(options_MiniCssExtractPlugin)
     ],
 
     module: 
@@ -67,7 +82,41 @@ module.exports = {
         [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: 
+                [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: 
+                        {
+                            postcssOptions: options_postCssLoader
+                        },
+                    }
+                ]
+            },
+            {
+                test: /\.modules.css$/,
+                use: 
+                [
+                    MiniCssExtractPlugin.loader, 
+                    {
+                        loader: 'css-loader',
+                        options: 
+                        {
+                            importLoaders: 1,
+                            modules: true,
+                            localIdentName: "[name]__[local]___[hash:base64:5]"
+                        }
+                    }, 
+                    {
+                        loader: "postcss-loader",
+                        options: 
+                        {
+                            postcssOptions: options_postCssLoader
+                        },
+                    }
+                ]
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
@@ -79,6 +128,5 @@ module.exports = {
             }
         ]
     }
-
 
 }
