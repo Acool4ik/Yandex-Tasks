e5ee99f295d1eef77fc5f288bbc7c5fbdcbb26f5
Acool4ik <byshkov.oleg@yandex.ru>
2021-02-22 20:22:53 +0300

    added babel-loader and @babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -81,6 +81,18 @@ module.exports = {
         rules: 
         [
             {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: 
+                {
+                    loader: "babel-loader",
+                    options: 
+                    {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.css$/,
                 use: 
                 [
